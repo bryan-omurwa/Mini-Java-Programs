421646dc306a777ef47b3ac14c66741cb584e255
Bryan Omurwa <bryanomurwa99@gmail.com>
2025-03-10 10:32:35 +0300

    Modified PatternChecker.java, and PrimeNumbers.java

--- src/main/java/math/operations/PatternChecker.java
+++ src/main/java/math/operations/PatternChecker.java
@@ -11,6 +11,8 @@ public class PatternChecker {
 
     private static boolean isPrime(int num) {
         if (num < 2) return false;
+
+        // We only check up to sqrt(num)
         for (int i = 2; i * i <= num; i++) {
             if (num % i == 0) return false;
         }
--- src/main/java/math/operations/PrimeNumbers.java
+++ src/main/java/math/operations/PrimeNumbers.java
@@ -1,17 +1,20 @@
 package math.operations;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class PrimeNumbers {
-    public ArrayList<Integer> getPrimeNumbers(int n) {
-        ArrayList<Integer> primeNumbers = new ArrayList<>();
+
+    /*
+    public List<Integer> getPrimeNumbers(int n) {
+        List<Integer> primeNumbers = new ArrayList<>();
         // If input is a negative number, throw an exception
         if (n < 0) {
             throw new IllegalArgumentException("Input must be a non-negative integer");
         }
 
         // If input number is 0 or 1, return an empty array
-        if (n == 1 || n == 0) {
+        if (n < 2) {
             return primeNumbers;
         }
         primeNumbers.add(2);
@@ -28,5 +31,43 @@ public class PrimeNumbers {
             }
         }
         return primeNumbers;
+    }
+
+     */ // Case one
+
+    public List<Integer> getPrimeNumbers(int n) {
+        List<Integer> primeNumbers = new ArrayList<>();
+
+        // If input is a negative number, throw an exception
+        if (n < 0) {
+            throw new IllegalArgumentException("Input must be a non-negative integer");
+        }
+
+        // If input number is 0 or 1, return an empty array
+        if (n < 2) {
+            return primeNumbers;
+        }
+
+        boolean[] isPrime = new boolean[n + 1];
+
+        for (int i = 2; i <= n; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        for (int k = 2; k <= n; k++) {
+            if (isPrime[k]) primeNumbers.add(k);
+        }
+
+        return primeNumbers;
+
     }
 }
